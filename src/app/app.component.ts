392f6b63de45537ec4c2e45e416904be52b93da2
phl3x0r <wispster@gmail.com>
2018-06-04 22:23:30 +0200

    add some material jazz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 import { RowerEventMessage } from '../models/rower-event-message.interface';
 import { ReplaySubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, startWith, tap } from 'rxjs/operators';
+import { MetricsService } from './services/metrics.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'Electron Rower';
-  private metrics$ = new ReplaySubject<RowerEventMessage>();
-
-  public speed$: Observable<number>;
-  public watt$: Observable<number>;
-  public distance$: Observable<number>;
+  private metricsSubject$ = new ReplaySubject<RowerEventMessage>();
+  public metrics$ = new Observable<RowerEventMessage>();
 
   constructor(private electron: ElectronService, private zone: NgZone) {}
 
@@ -23,12 +21,12 @@ export class AppComponent implements OnInit {
     this.electron.ipcRenderer.on(
       'rowerEvent',
       (_event: any, message: RowerEventMessage) => {
-        this.zone.run(() => this.metrics$.next(message));
-        console.log(message);
+        this.zone.run(() => this.metricsSubject$.next(message));
       }
     );
-    this.speed$ = this.metrics$.pipe(map(m => m.speed));
-    this.watt$ = this.metrics$.pipe(map(m => m.watt));
-    this.distance$ = this.metrics$.pipe(map(m => m.distance));
+    this.metrics$ = this.metricsSubject$.asObservable().pipe(
+      startWith(<RowerEventMessage>{ speed: 0, watt: 0, distance: 0 }),
+      tap(metrics => console.log(metrics))
+    );
   }
 }
